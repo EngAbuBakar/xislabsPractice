7b79e5b04d594b6eb6e47cd730afd44d64f74d30
xlw05 <abubakaryameen0243@gmail.com>
2023-06-21 12:08:07 +0500

    3rd Commit

---
+++ src/ProblemPrctice/ReverseString.java
@@ -0,0 +1,31 @@
+package ProblemPrctice;
+
+public class ReverseString {
+    public static void main(String[] args) {
+    //reverseString("Hello world!");
+    reverseNumber(2324);
+    }
+
+    public static String reverseString(String name) {
+        //  String name="abu bakar";
+        String nstr = "";
+
+        for (int i = name.length() - 1; i >= 0; i--) {
+            nstr = nstr + name.charAt(i);
+        }
+        System.out.println(nstr);
+        return nstr;
+
+
+    }
+    public  static int reverseNumber(int num){
+       int revNum=0;
+       int rem=0;
+       while (num!=0){
+          rem=num%10;
+          revNum=revNum*10+rem;
+          num=num/10;
+       }
+        return revNum;
+    }
+}
---
+++ src/ProblemPrctice/SortingArray.java
@@ -0,0 +1,45 @@
+package ProblemPrctice;
+
+import java.util.Arrays;
+
+public class SortingArray {
+    public static void main(String[] args) {
+        int [] numbers={4,2,1,4,5,8};
+        int temp=0;
+        for (int i = 0; i <numbers.length; i++) {
+            for (int j = i+1; j <numbers.length ; j++) {
+                if (numbers[i]>numbers[j]){
+                    temp=numbers[i];
+                    numbers[i]=numbers[j];
+                    numbers[j]=temp;
+                }
+            }
+
+        }
+
+        System.out.println("Ascending order: ");
+        for (int i = 0; i <=numbers.length ; i++) {
+            System.out.println(numbers[i]);
+
+        }
+
+         int [] num={4,3,2,6,8};
+        for (int i = 0; i <num.length; i++) {
+            for (int j = i+1; j <num.length ; j++) {
+                if (num[i]<num[j]){
+                    temp=num[i];
+                    num[i]=num[j];
+                    num[j]=temp;
+                }
+            }
+
+        }
+
+        System.out.println("Descending order: ");
+        for (int i = 0; i <=num.length ; i++) {
+            System.out.println(num[i]);
+
+        }
+
+    }
+}
